c29e2605ca00dfd059c74621613d696675856de0
jlovera3 <jloveraulecia@gmail.com>
2021-03-03 20:05:44 +0100

    empezando con login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
     path: 'gastos',
     loadChildren: () => import('./pages/gastos/gastos.module').then( m => m.GastosPageModule)
   },
@@ -10,10 +19,6 @@ const routes: Routes = [
     path: 'socios',
     loadChildren: () => import('./pages/socios/socios.module').then( m => m.SociosPageModule)
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  }
 ];
 @NgModule({
   imports: [
